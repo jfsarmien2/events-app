9a323e47918c6b1db52e20ca46b0795fab6a9478
John <john.sarmiento452@gmail.com>
2021-07-06 16:55:25 +0800

    delete event function

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,15 +1,31 @@
 import {FaPencilAlt, FaTimes} from 'react-icons/fa'
 import Link from 'next/link'
+import {ToastContainer, toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Image from 'next/image'
 import Layout from '@/components/Layout'
 import { API_URL } from '@/config/index'
 import styles from '@/styles/Event.module.css'
-
+import { useRouter } from 'next/router'
+import { route } from 'next/dist/next-server/server/router'
 
 export default function EventPage({evt}) {
+    const router = useRouter();
 
-    const deleteEvent = (e) => {
-        console.log('delete');
+    const deleteEvent = async (e) => {
+        if(confirm('Are you sure you want to delete?')) {
+           const res = await fetch(`${API_URL}/events/${evt.id}`, {
+            method: 'DELETE',
+           })
+
+           const data = await res.json();
+           if(!res.ok) {
+              toast.error(data.message);
+              return;
+           } else {
+               router.push('/events');
+           }
+        }
     }
     return (
         <Layout>
@@ -26,7 +42,7 @@ export default function EventPage({evt}) {
                 <span>
                     {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
                 </span>
-
+                 <ToastContainer />
                 <h1>{evt.name}</h1>
                 
                 {evt.image && (
